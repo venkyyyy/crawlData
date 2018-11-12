b75b2d5c84e0550099f1de7ee86bbd870961a6a1
venkyyyy <venkyanthony@gmail.com>
2018-11-12 15:16:09 +0530

    Update crawl_data.py

--- crawlSpider/spiders/crawl_data.py
+++ crawlSpider/spiders/crawl_data.py
@@ -4,11 +4,11 @@ from scrapy_splash import SplashRequest
 
 class Crawl_data(scrapy.Spider):
     name = "crawl_data"
-    start_urls = ["http://example.com"]
+    start_urls = ["https://www.google.co.in/search?q=Republic+Services+in+U.S.&ibp=htl;jobs"]
 
     def start_requests(self):
         for url in self.start_urls:
             yield SplashRequest(url, self.parse, args={'wait': 0.5})
 
     def parse(self, response):
-        yield response.body
+        yield {response.body}
